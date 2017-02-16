ab0b0ae606197c23a0cbc8cd435b66d4ea256134
Jordan Huang <good5dog5@gmail.com>
2017-02-16 16:36:38 +0800

    Stable version1.

--- crawler/course.py
+++ crawler/course.py
@@ -5,19 +5,41 @@
 import os
 import sys
 import subprocess
-
-from urllib.request import urlopen
-
-from itertools import zip_longest
 import requests
 import lxml.html
 import re
+from itertools import zip_longest
+
+from utils.config import get_config_section
+from config import course_dict
+
+crawler_config      = get_config_section('crawler')
+encoding            = crawler_config['encoding']  # big5 superset
+
+form_url            = crawler_config['form_url']
+form_action_url     = crawler_config['form_action_url']
+syllabus_url        = crawler_config['syllabus_url']
+attachment_url      = crawler_config['attachment_url']
+dept_url            = crawler_config['dept_url']
 
 
 def xpath0(element, xpath):
     result = element.xpath(xpath)
     assert len(result) == 1, result
     return result[0]
+
+
+def extract_text(element, joiner=''):
+    return joiner.join(element.itertext()).replace(" ", "")
+
+
+def extract_int(element, allow_empty=False):
+    text_list = element.xpath('.//text()')
+    assert len(text_list) == 1, text_list
+    if allow_empty and not text_list[0].strip():
+        return None
+    return int(text_list[0])
+
 
 def extract_multirow_text(element):
     return re.sub(
@@ -28,8 +50,99 @@ def extract_multirow_text(element):
         )
     )
 
-def coursedata_from_syllabus(filename):
 
+class EmptyResponse(Exception):
+    pass
+def with_retry(request_function):
+    def function(url, max_retries=32, **kwargs):
+        '''
+        get a valid response in <max_retries> retries
+        change encoding before return
+        raises EmptyResponse if not valid
+        '''
+        for r in range(max_retries):
+            response = request_function(url, **kwargs)
+            if response.content:
+                response.encoding = encoding
+                return response
+        raise EmptyResponse(url)
+    function.__name__ = request_function.__name__
+    return function
+
+
+
+def get_slfr(text):
+    sl, s, fr = text.partition(u'新生保留')
+    if not sl:
+        return None, 0
+    sl = int(sl)
+    if fr:
+        assert fr[-1] == u'人', fr
+        fr = int(fr[:-1])
+    else:
+        fr = 0
+    return sl, fr
+
+
+get = with_retry(requests.get)
+post = with_retry(requests.post)
+
+def curriculum_to_trs(html):
+    document = lxml.html.fromstring(html)
+    course_trs = document.xpath("//tr[contains(@class, 'class3')]")
+    assert len(course_trs) % 2 == 0, len(course_trs)
+    return course_trs[::2]
+
+
+def course_from_tr(main_tr):
+    '''
+    main_tr -> dict: course data
+
+    data info: ([x] means implemented but disabled)
+    no                  text            course number
+    name_zh             text            Chinese course name
+    name_en             text / None     English course name
+    ge_hint             text / None     GE line text
+    credit [x]          int             credit
+    time [x]            text            time
+    rc [x]              text            room & capacity
+    teacher             text            teacher name, may contain English
+    size_limit          int / None      quota size limit
+    fr                  int             quota reserved for freshmen
+    note                text            note
+    enrollment          int             quota enrollment
+    object              text            object
+    has_prerequisite    bool            has prerequisite
+    '''
+    tds = main_tr.xpath('td')
+    part = {
+        'no': extract_text(tds[0]),
+        'credit': extract_int(tds[2]),
+        'time': extract_text(tds[3]),
+        'room/capacity': extract_text(tds[4]),
+        'teacher': extract_text(tds[5]),
+        'note': extract_text(tds[7]),
+        'enrollment': extract_int(tds[8]),
+        'object': extract_text(tds[9]),
+    }
+    part['size_limit'], part['fr'] = get_slfr(extract_text(tds[6]))
+    for key, text in zip_longest(
+        ('name_zh', 'name_en', 'ge_hint'),
+        tds[1].itertext(),
+    ):
+        if text is not None:
+            text = text.strip()
+        part[key] = text
+    prerequisite = extract_text(tds[10])
+    if prerequisite == u'擋修' or u'先修科目' in prerequisite:
+        part['has_prerequisite'] = True
+    else:
+        assert not prerequisite, 'unknown prerequisite %r' % prerequisite
+        part['has_prerequisite'] = False
+    return part
+
+
+def course_from_syllabus(html):
     '''
     syllabus html -> dict: course data
 
@@ -47,15 +160,13 @@ def coursedata_from_syllabus(filename):
     https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/output/6_6.1_6.1.12/<no>.pdf
     where <no> is the course number
     '''
-
-    html = open(filename, "r").read()
-    document = lxml.html.fromstring(html)
-
-    def extract_text(element, joiner=''):
-        return joiner.join(element.itertext()).strip()
+    document = lxml.html.fromstring(html,parser = lxml.etree.HTMLParser())
 
     def xpath_text(xpath, joiner=''):
         return extract_text(xpath0(document, xpath), joiner=joiner)
+
+    def xpath_int(xpath):
+        return extract_int(xpath0(document, xpath))
 
     def patch_teacher(text):
         '''
@@ -65,7 +176,6 @@ def coursedata_from_syllabus(filename):
         '''
         return re.sub(r'\([^)]*\)', '', text)
 
-    
     return {
         'no': xpath_text('/html/body/div/table[1]/tr[2]/td[2]'),
         'name_zh': xpath_text('/html/body/div/table[1]/tr[3]/td[2]'),
@@ -77,13 +187,38 @@ def coursedata_from_syllabus(filename):
         'room': xpath_text('/html/body/div/table[1]/tr[6]/td[4]', joiner=', '),
         'syllabus': extract_multirow_text(
             xpath0(document, '/html/body/div/table[5]/tr[2]/td')),
-        'has_attachment': bool(
-            document.xpath('/html/body/div/table[4]/tr[2]/td/div/font[1]/a')),
+        'has_attachment': bool( document.xpath('/html/body/div/table[5]/tr[2]/td/div/font[1]/a')),
+        'attachment_url': document.xpath('/html/body/div/table[5]/tr[2]/td/div/font[1]/a/@href')
     }
+
+
+def get_syllabus(c_key, acixstore):
+    return get(syllabus_url, params={'c_key': c_key, 'ACIXSTORE': acixstore})
+
+
+def get_cou_codes(url):
+    html = get(form_url).text
+    document = lxml.html.fromstring(html)
+    return document.xpath('//select[@name="cou_code"]/option/@value')
+
+def gen_cou_codes_dict(url):
+    html = get(form_url).text
+    document = lxml.html.fromstring(html)
+    # print( document.xpath('//select[@name="cou_code"]/option/@value'))
+    print( [x.text for x in document.xpath('//select[@name="cou_code"]/option')])
 
 if __name__ == '__main__':
 
-    info = coursedata_from_syllabus("./syllabus.html")
-    print(info.items())
-    print(info.get("syllabus", "None"))
+    course_dict = coursedata_from_syllabus(sys.argv[1])
+
+    Semester = "1051"
+    No          = course_dict['no']
+    Name        = course_dict['name_zh']
+    Syllabus    = course_dict['syllabus']
+    Department  = course_code[re.sub("[0-9]", "", No.strip())]
+
+    fileName = "-".join(s for s in [Semester, Department, No, Name])+".txt"
+
+    f = open(fileName, "w")
+    f.write(Syllabus)
     
--- get_namelist.py
+++ get_namelist.py
@@ -5,17 +5,175 @@ import re
 import os
 import sys
 import subprocess
-from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("./index_table.html"),"lxml")
+from urllib.request import urlopen
+import requests
+from requests_futures.sessions import FuturesSession
+
+from lxml import html, etree
+from crawler.crawler import crawl_course, crawl_dept
+from crawler.course import gen_cou_codes_dict, course_from_syllabus
+
+try:
+    from crawler.decaptcha import Entrance, DecaptchaFailure
+except ImportError:
+    Entrance = None
+
+from config import cou_codes, course_url, year_semester_dict, id_2_pass_list, ROOT_DIR
+import csv 
+import logging
+
+def with_retry(request_function):
+    def function(url, max_retries=32, **kwargs):
+        '''
+        get a valid response in <max_retries> retries
+        change encoding before return
+        raises EmptyResponse if not valid
+        '''
+        for r in range(max_retries):
+            response = request_function(url, **kwargs)
+            if response.content:
+                return response
+        raise EmptyResponse(url)
+    function.__name__ = request_function.__name__
+    return function
+
+get = with_retry(requests.get)
+post = with_retry(requests.post)
+
+def get_auth_pair(url):
+    if Entrance is not None:
+        try:
+            return Entrance(url).get_ticket()
+        except DecaptchaFailure:
+            print('Automated decaptcha failed.')
+    else:
+        print('crawler.decaptcha not available (requires tesseract >= 3.03).')
+    print('Please provide valid ACIXSTORE and auth_num from')
+    print(url)
+    ACIXSTORE = input('ACIXSTORE: ')
+    auth_num = input('auth_num: ')
+    return ACIXSTORE, auth_num
+
 
 def chinese_word_only(text):
 
     # trim digits,letters and newline
-    pattern = "[A-Za-z0-9\[\`\~\!\@\#\$\^\&\*\(\)\=\|\{\}\'\:\;\'\,\[\]\.\<\>\/\?\~\！\@\#\\\&\*\%\n]" 
+    pattern = "[A-Za-z0-9\[\`\~\!\@\#\$\^\&\*\(\)\=\|\{\}\'\:\;\'\,\[\]\.\<\>\/\?\~\！\@\#\\\&\*\%\n\ ]" 
     return re.sub(pattern,"", text)
 
+
+def get_course_no_list(treeObj):
+    results = treeObj.findall(".//table/tr[@class='class3']/td[1]/div[@align='center']")
+    return results
+
+def gen_file_name(ys, cou_dict):
+
+    dept = cou_codes[re.sub("[0-9]", "", cou_dict['no'].strip())]
+    return "-".join(s for s in [ys, dept, cou_dict['no'], cou_dict['name_zh']]).replace("/", "-")
+
+def syllabus_from_curriculum(acixstore, cou_no):
+    data = { 'ACIXSTORE': acixstore, 'c_key': cou_no }
+    req = get(course_url['syllabus'], params=data)
+    req.encoding = "cp950"
+    return req
+
+def cou_code_2_curriculum(session, acixstore, cou_code, auth_num, ys):
+
+    return  session.post(
+        course_url['curriculum'],
+        data = {
+            'ACIXSTORE' : acixstore,
+            'YS' : ys,
+            'cond': 'b',
+            'dept' : cou_code,
+            'auth_num': auth_num })
+
+def download_syllabus_file(path, req, cou_dict, filename):
+    
+    fName = ""
+
+    if cou_dict['has_attachment']:
+
+        fName = "".join([filename, ".pdf"])
+        full_path = os.path.join(path, fName)
+        pdf_url = "https://www.ccxp.nthu.edu.tw/" + cou_dict['attachment_url'][0]
+        res = get(pdf_url)
+
+        with open(full_path,"wb") as pdf:
+            pdf.write(res.content)
+
+        print ("Create {0}".format(fName))
+
+    else: 
+        fName = "".join([filename, ".txt"])
+        full_path = os.path.join(path, fName)
+
+        with open(full_path, "w") as txt:
+            txt.write(cou_dict["syllabus"])
+
+        print ("Create {0}".format(fName))
+        
+    return fName
+
+def keywordAnalyser(fname):
+
+    with open(fname, 'r') as f:
+        content = f.read().replace('\n', '')
+        wordfreq = [len(re.findall(keyword, content)) for keyword in keyword_list]
+        print(wordfreq)
+
+    return OrderedDict(zip(keyword_list, wordfreq))
+
 if __name__ == '__main__':
-    results = soup.findAll("td", {"width" : "20%"})
-    for a in results:
-        print(chinese_word_only(a.text))
+
+    acixstore, auth_num = get_auth_pair(course_url['curriculum_entry'])
+
+    with FuturesSession(max_workers=1) as session:
+
+        for year_semester in sorted(year_semester_dict.keys()):
+
+            folder = os.path.join("./syllabus_download", year_semester_dict[year_semester])
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+            log = open( os.path.join(folder, "log"), "w")
+            log_csv = open( os.path.join(folder, "log.csv"), "w")
+
+            for cou_code in cou_codes.keys():
+
+                curriculum = cou_code_2_curriculum(session, acixstore, cou_code, auth_num, year_semester) 
+                curriculum_req = curriculum.result()
+                curriculum_req.encoding = "cp950"
+                curriculum_text = html.fromstring(curriculum_req.text, parser=etree.HTMLParser())
+
+                course_no_list   = get_course_no_list(curriculum_text)
+                for no in course_no_list:
+
+                    if no.text in id_2_pass_list:
+                        print("{0} been passed".format(no.text))
+                        continue
+
+                    syllabus_req = syllabus_from_curriculum(acixstore, no.text)
+                    ques = open("./ques", "w")
+                    print(syllabus_req.text, no.text, file=ques)
+                    cou_dict = course_from_syllabus(syllabus_req.text)
+                    syllabus_file_name = gen_file_name(year_semester_dict[year_semester], cou_dict)
+                    # print(cou_codes[re.sub("[0-9]", "", cou_dict['no'].strip())], file=log)
+
+                    fName = download_syllabus_file(folder, syllabus_req, cou_dict, syllabus_file_name )
+                    print("{0:>10} {1:>30} {2:>50}".format(cou_codes[cou_code], cou_dict['name_zh'], fName), file=log)
+
+                    w = csv.writer(log_csv, delimiter=',')
+
+                    data = [cou_codes[cou_code], cou_dict['name_zh'], '', fName]
+                    w.writerow(data)
+                    ques.close()
+
+
+            log.close()
+            log_csv.close()
+
+    
+
+
